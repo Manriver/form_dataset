2163d71c11bdcec0a4482e818ddb1682485a9254
Mario Rivera <maro.arivera@yahoo.com>
2017-10-31 19:41:42 -0600

    se agrega una tabla para presentar las capas

--- main.js
+++ main.js
@@ -14,7 +14,9 @@ $(function(){
         });//Fin .on("done",function(data){
 
         $("#enviar").on("click",function(evt){
-
+        	// console.log(this);
+        	var $this = $(this);
+        	$this.button('loading');
             var optiondelegacion = $("#selectDel").val();
             var optionnamedataset = $("#inputdataset").val();
             var optionnamecapa = $("#inputcapa").val();
@@ -30,23 +32,45 @@ $(function(){
 			            var sustituir = /'/g;
 			            var sustituto = "";
 	            		var nuevofields = field_string.replace(sustituir, sustituto);
-	            		// console.log(nuevofields);
 	            		$('#fields').val(nuevofields);
 
 			            var optionfields = $("#fields").val();
 
 			            var strsqlinsert ="INSERT INTO "+'"finanzasdf-admin"'+".sismo_categoria_capa_1 (categoria, dataset, fields, nombre_de_capa) VALUES ('"+optiondelegacion+"','"+optionnamedataset+"','"+nuevofields+"','"+optionnamecapa+"')" ;
 			            console.log(strsqlinsert);
-			            $.getJSON("https://finanzasdf-admin.carto.com/api/v2/sql?q="+strsqlinsert+"&api_key=9636b95f3e6c01a780c65a869e491303ed330f9c", function(data){
-			                 $.each(data.rows, function(key, val) {
-			                     alert("llave "+key+" "+val);
-			                 });
-			            });//end getJSON
-
+			            // $.getJSON("https://finanzasdf-admin.carto.com/api/v2/sql?q="+strsqlinsert+"&api_key=9636b95f3e6c01a780c65a869e491303ed330f9c", function(data){
+			            //      $.each(data.rows, function(key, val) {
+			            //          alert("llave "+key+" "+val);
+			            //      });
+			            // });//end getJSON
 	            	}//fin for
+            	$this.button('reset');
 
+            	}).error(function(errors){
+            		$this.button('reset');
+            		console.log("errors: "+errors);
             	});//fin getJSON
-
+            	setTimeout(function(){
+            		$("<p />",{"text":"Datos enviados"}).appendTo('#textresult');
+            	}, 3000);    
         });//fin  on click
 
+        $('#selectDel').on("change",function(event) {
+        	/* Act on the event */
+        	var optionDel = $("#selectDel").val();
+			$("#detalledatset").empty();
+
+			objsql.execute("SELECT cartodb_id, categoria, dataset, nombre_de_capa FROM sismo_categoria_capa_1 WHERE categoria = '"+optionDel+"'")
+			.done(function(data){
+
+				$("#detalledatset").html("");
+				for(idx in data.rows){
+					$tr = $("<tr>",{"id":data.rows[idx].cartodb_id});
+					$("<td />",{"text":data.rows[idx].nombre_de_capa}).appendTo($tr);
+					$("<td />",{"text":data.rows[idx].dataset}).appendTo($tr);
+					$tr.appendTo("#detalledatset");
+				}
+
+			});      	
+        });
 });
